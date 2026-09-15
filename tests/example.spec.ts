f61fbee03c18ea530aaabea7dc0a2c925d9178ad
davidd26x <jose01265@gmail.com>
2026-09-14 21:58:39 -0600

    test: actualiza verificaciones en el archivo de pruebas

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,14 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+//Tests de pruebas para practicar en playwright
+
+test('toggle dark mode', async ({ page }) => {
   await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  
+  // Buscar y hacer clic en el botón de cambiar tema
+  const themeButton = page.getByLabel(/Switch between dark and light mode/i);
+  await themeButton.click();
+  
+  // Verificar que el atributo html cambie a 'dark' o 'light'
+  await expect(page.locator('html')).toHaveAttribute('data-theme', /dark|light/);
 });
